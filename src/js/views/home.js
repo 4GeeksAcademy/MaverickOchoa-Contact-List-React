0b128fd70796f5bc32d26affd0a0c6366b61041b
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-07-05 19:49:33 +0000

    started the proyect

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,41 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="text-center container-fluid m-5">
+		
+		<div className="cards">
+			<div>
+				<img src="https://pics.craiyon.com/2023-07-31/bb9ac57c48ed4535ba0855f9dcc5097f.webp" class="card-img-top" alt="..."/>
+			</div>
+			
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+		<div className="cards">
+			<div>
+				<img src="https://pics.craiyon.com/2023-07-31/bb9ac57c48ed4535ba0855f9dcc5097f.webp" class="card-img-top" alt="..."/>
+			</div>
+			
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+		<div className="cards">
+			<div>
+				<img src="https://pics.craiyon.com/2023-07-31/bb9ac57c48ed4535ba0855f9dcc5097f.webp" class="card-img-top" alt="..."/>
+			</div>
+			
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+		
 	</div>
 );
